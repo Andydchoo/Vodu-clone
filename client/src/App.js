d00b3dad5639184d85510306776872d23e04e784
Crazynuget <andydchoo@gmail.com>
2023-03-13 22:50:12 -0400

    messing with uri

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import Footer from "./components/Footer";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: "https://vod-u-front.onrender.com/graphql",
+  uri: "https://happy-nightshirt-wasp.cyclic.app/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
